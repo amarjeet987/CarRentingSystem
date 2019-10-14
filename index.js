d8338b1c06f17d34f89c0601517f0ff1878ce83c
Amarjeet Sarma <38986305+amarjeet987@users.noreply.github.com>
2019-10-15 03:42:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/book', require('./routes/api/booking'));
 
 // server
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
